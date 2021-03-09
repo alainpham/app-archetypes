9b209889afd74d804113de68ce4cd5f121b0622c
Alain Pham <alainphm@gmail.com>
2021-03-09 13:19:34 +0100

    camel monitoring

---
+++ quarkus-library/src/main/resources/archetype-resources/ReadMe.md
@@ -0,0 +1,3 @@
+#[[# Project]]# ${artifactId}
+
+This project uses Quarkus Hibernate ORM to build base business model objects. Should act as common library.
---
+++ quarkus-library/src/main/resources/archetype-resources/src/main/java/Main.java
@@ -0,0 +1,32 @@
+package ${package};
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.apache.camel.builder.RouteBuilder;
+// import org.apache.camel.model.rest.RestBindingMode;
+
+@ApplicationScoped
+public class Main extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        
+
+		// restConfiguration()
+		// .apiContextRouteId("api-docs")
+		// .bindingMode(RestBindingMode.json)
+		// .contextPath("/camel")
+		// .apiContextPath("/api-docs")
+		// .apiProperty("cors", "true")
+		// .apiProperty("api.title", "test")
+		// .apiProperty("api.version", "test")
+		// .dataFormatProperty("prettyPrint", "true");
+        // ;
+
+
+        from("timer:test-from-java?period=1s").log("hello from java");
+
+        
+    }
+    
+}
