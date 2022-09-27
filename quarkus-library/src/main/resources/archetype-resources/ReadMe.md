5b0c6d226edf74a2a41ac16fb77fab3fd0a51dbc
alainpham <alainphm@gmail.com>
2022-09-28 00:55:36 +0200

    removed quarkus library

--- quarkus-library/src/main/resources/archetype-resources/ReadMe.md
+++
@@ -1,3 +0,0 @@
-#[[# Project]]# ${artifactId}
-
-This project uses Quarkus Hibernate ORM to build base business model objects. Should act as common library.
--- quarkus-library/src/main/resources/archetype-resources/src/test/java/NativeMainResourceIT.java
+++
@@ -1,9 +0,0 @@
-package ${package};
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeMainResourceIT extends MainResourceTest {
-
-    // Execute the same tests but in native mode.
-}
